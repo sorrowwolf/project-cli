836b56ab5ef4a160ded54879e8a80563eb411c4e
sorrowwolf <414471841@qq.com>
2022-02-07 21:52:08 +0800

    change

---
+++ core/cli/lib/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = core;
+
+const pkg = require("../package.json");
+
+function core() {
+    // TODO
+    console.log('core')
+}
+
+
+function checkPkgVersion() {
+    const version = pkg.version;
+}
