32432273fc279ecdaa07973fdea27fcb4e18277b
sorrowwolf <414471841@qq.com>
2022-02-21 22:19:16 +0800

    5-3-3

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const Command = require("@sorrow-cli-dev/command");
 const log = require('@sorrow-cli-dev/log');
 
@@ -11,7 +12,32 @@ class InitCommand extends Command {
         log.verbose('force', this.force);
     }
     exec() {
-        console.log('exec');
+        try {
+            // 1. 准备阶段
+            this.prepare();
+            // 2. 下载模板
+            // 3. 安装模板
+        } catch (e) {
+            log.error(e.message);
+        }
+    }
+    prepare() {
+        // 1. 判断当前目录是否为空
+        const ret = this.isCwdEmpty();
+        if (!ret) {
+            // 1.1 询问是否继续创建
+        }
+        // 2. 是否启动强制更新
+        // 3. 选择创建项目或组件
+        // 4. 获取项目的基本信息
+    }
+    isCwdEmpty() {
+        const localPath = process.cwd();
+        let fileList = fs.readdirSync(localPath);
+        fileList = fileList.filter(file => (
+            !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+        ));
+        return !fileList || fileList.length <= 0;
     }
 }
 
